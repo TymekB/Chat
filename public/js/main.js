4baae3d927cd873955fa06e6f742eaa6659975bc
TymekB <baran.tymoteusz@gmail.com>
2018-12-30 18:23:03 +0100

    added event listener to send button

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,13 @@ let socket = io();
 
 $(document).ready(function(){
 
+    $("#btn-send").click(function(){
 
+        let messegeInput = $("#messege-input");
+
+        console.log(messegeInput.val());
+
+        messegeInput.val('');
+    });
 
 });
